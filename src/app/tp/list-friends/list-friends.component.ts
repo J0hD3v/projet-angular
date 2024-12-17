cbf9279e23462f25bd77f7c99ecf1bb41b8ef81d
J0hD3v <johanfievet@orange.fr>
2024-12-17 16:44:47 +0100

    ngIf et ngFor / 2eme patch tp data binding

--- src/app/tp/list-friends/list-friends.component.ts
+++ src/app/tp/list-friends/list-friends.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { OneFriendComponent } from './one-friend/one-friend.component';
-import { FormsModule } from '@angular/forms'; // permet le 2 way binding avec ngModel
+import { FormsModule } from '@angular/forms'; // permet d'utiliser ngModel
+import { NgIf } from '@angular/common'; // permet d'utiliser ngIf
+import { NgFor } from '@angular/common'; // permet d'utiliser ngFor
 
 @Component({
   selector: 'app-list-friends',
   imports: [
     OneFriendComponent,
     FormsModule,
+    NgIf,
+    NgFor
   ],
   templateUrl: './list-friends.component.html',
   styleUrl: './list-friends.component.css'
@@ -22,16 +26,19 @@ export class ListFriendsComponent {
   // question 1
   listFriendsCreationStatus:string="rien"
   updateStatus(): void {
-    this.listFriendsCreationStatus = "Félicitations ! 🎁"
-  }
-
-  // question 2
-  inputCopie:string = "vide"
-  onInput(event:Event): void {
-    console.log(event);
-    this.inputCopie = (<HTMLInputElement>event.target).value
+    this.listFriendCreated = true;
   }
 
   // avec ngModel
   uneData:string = ""
+
+  // avec ngIf
+  listFriendCreated:boolean = false
+
+  // avec ngFor
+  listFriendsTab: { name: string; age: number; email: string }[] = [
+    { name: 'Alice', age: 30, email: 'alice@example.com' },
+    { name: 'Bob', age: 25, email: 'bob@example.com' },
+    { name: 'Charlie', age: 35, email: 'charlie@example.com' }
+  ]
 }
