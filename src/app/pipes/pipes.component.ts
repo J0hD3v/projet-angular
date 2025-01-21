ee9523c766cb3eab1cac2772653e3bade76500ca
J0hD3v <johanfievet@orange.fr>
2025-01-21 12:22:13 +0100

    ajout de pipes personnalisés

--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -1,10 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Pipe } from '@angular/core';
+
+@Pipe ({
+  name: 'reverseText',
+})
+export class ReverseText {
+  transform(value: string): string {
+    return value.split("").reverse().join("");
+  }
+}
+
+@Pipe ({
+  name: 'funText',
+})
+export class FunText {
+  transform(value: string): string {
+    return value != "" ? ("😄 " + value + " 😄") : "";
+  }
+}
 
 @Component({
   selector: 'app-pipes',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,ReverseText,FunText],
   templateUrl: './pipes.component.html',
   // styleUrl: './pipes.component.css'
   template:`
@@ -18,6 +37,7 @@ import { FormsModule } from '@angular/forms';
       float: right;
     }`]
 })
+
 export class PipesComponent {
   currentDate: Date = new Date();
   exampleText: string = 'Angular Pipes';
@@ -25,4 +45,8 @@ export class PipesComponent {
   decimalValue: number = 42.56789;
   jsonData = { name: 'Angular', version: 19 };
   interactiveText: string = 'Hello, Angular!';
+
+  sampleText: string = 'Angular Pipes';
+  customText: string = '';
+  customPrice: number = 0;
 }
