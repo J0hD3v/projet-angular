58947a33f618d7704ce1659af38d1c18ccdfa7c7
J0hD3v <johanfievet@orange.fr>
2025-01-22 14:38:16 +0100

    jeu pokemon fonctionnel

--- src/app/pokemon-game/pokemon-game.component.ts
+++ src/app/pokemon-game/pokemon-game.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
+import { NgStyle } from '@angular/common';
 
 interface logMessage {
   id: number | null,
@@ -9,7 +11,11 @@ interface logMessage {
 
 @Component({
   selector: 'app-pokemon-game',
-  imports: [],
+  imports: [
+    NgIf,
+    NgFor,
+    NgStyle,
+  ],
   templateUrl: './pokemon-game.component.html',
   styleUrl: './pokemon-game.component.css'
 })
@@ -20,38 +26,34 @@ export class PokemonGameComponent {
   playerHealth:any = {value:100};
   monsterHealth:any = {value:100};
   currentRound:any = {value:0};
-  winner:any = {value:""};
-  logMessage:logMessage = {
-    id: null,
-    actionBy: null,
-    actionType: null,
-    actionValue: null,
-  };
+  winner:string | null = "";
+  logMessages:Array<logMessage> = [];
 
   getRandomValue = (min: number, max: number): number => Math.floor(Math.random() * (max - min)) + min;
 
 
-  monsterBarStyles = {
-      width: this.monsterHealth.value > 0 ? `${this.monsterHealth.value}%` : "0%",
+  monsterBarStyles() {
+    return {
+      width: this.monsterHealth.value > 0 ? `${this.monsterHealth.value}%` : "0%"
+    }
   };
 
-  playerBarStyles = {
+  playerBarStyles() {
+    return {
       width: this.playerHealth.value > 0 ? `${this.playerHealth.value}%` : "0%",
+    }
   };
 
-  mayUseSpecialAttack = this.currentRound.value % 3 !== 0;
+  mayUseSpecialAttack() {
+    return this.currentRound.value % 3 == 0;
+  }
 
   startGame = () => {
     this.playerHealth.value = 100;
     this.monsterHealth.value = 100;
-    this.winner.value = "";
+    this.winner = null;
     this.currentRound.value = 0;
-    this.logMessage = {
-      id: null,
-      actionBy: null,
-      actionType: null,
-      actionValue: null,
-    }; // Clear the log
+    this.logMessages.splice(0, this.logMessages.length); // Clear the log
   };
 
 
@@ -59,6 +61,11 @@ export class PokemonGameComponent {
     this.currentRound.value++;
     const attackValue = this.getRandomValue(5, 12);
     this.monsterHealth.value -= attackValue;
+    if (this.monsterHealth.value <= 0 && this.playerHealth.value <= 0) {
+      this.winner = "draw";
+    } else if (this.monsterHealth.value <= 0) {
+      this.winner = "player";
+    }
     this.addLogMessage("player", "attack", attackValue);
     this.attackPlayer();
   };
@@ -73,6 +80,11 @@ export class PokemonGameComponent {
     this.currentRound.value++;
     const attackValue = this.getRandomValue(10, 25);
     this.monsterHealth.value -= attackValue;
+    if (this.monsterHealth.value <= 0 && this.playerHealth.value <= 0) {
+      this.winner = "draw";
+    } else if (this.monsterHealth.value <= 0) {
+      this.winner = "player";
+    }
     this.addLogMessage("player", "attack", attackValue);
     this.attackPlayer();
   };
@@ -86,16 +98,16 @@ export class PokemonGameComponent {
   };
 
   surrender = () => {
-    this.winner.value = "monster";
+    this.winner = "monster";
   };
 
   addLogMessage = (actionBy: "player" | "monster", actionType: "attack" | "heal", actionValue: number) => {
-    this.logMessage = {
+    this.logMessages.push({
         id: Date.now(),
         actionBy,
         actionType,
         actionValue,
-    };
+    });
   };
 
 }
